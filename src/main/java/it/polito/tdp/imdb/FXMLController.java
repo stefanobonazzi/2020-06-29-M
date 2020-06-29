8008f3bc0e148730bfaab034fbb3ea7601df1f15
tattommasi <tatiana.tommasi@polito.it>
2020-06-29 02:23:56 +0200

    upload

---
+++ src/main/java/it/polito/tdp/imdb/FXMLController.java
@@ -0,0 +1,82 @@
+/**
+ * Sample Skeleton for 'Scene.fxml' Controller Class
+ */
+
+package it.polito.tdp.imdb;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import it.polito.tdp.imdb.model.Model;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class FXMLController {
+	
+	private Model model;
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="btnCreaGrafo"
+    private Button btnCreaGrafo; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnAdiacenti"
+    private Button btnAdiacenti; // Value injected by FXMLLoader
+
+    @FXML // fx:id="btnCercaAffini"
+    private Button btnCercaAffini; // Value injected by FXMLLoader
+
+    @FXML // fx:id="boxAnno"
+    private ComboBox<?> boxAnno; // Value injected by FXMLLoader
+
+    @FXML // fx:id="boxRegista"
+    private ComboBox<?> boxRegista; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtAttoriCondivisi"
+    private TextField txtAttoriCondivisi; // Value injected by FXMLLoader
+
+    @FXML // fx:id="txtResult"
+    private TextArea txtResult; // Value injected by FXMLLoader
+
+    @FXML
+    void doCreaGrafo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doRegistiAdiacenti(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doRicorsione(ActionEvent event) {
+
+    }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert btnCreaGrafo != null : "fx:id=\"btnCreaGrafo\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnAdiacenti != null : "fx:id=\"btnAdiacenti\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnCercaAffini != null : "fx:id=\"btnCercaAffini\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert boxAnno != null : "fx:id=\"boxAnno\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert boxRegista != null : "fx:id=\"boxRegista\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtAttoriCondivisi != null : "fx:id=\"txtAttoriCondivisi\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
+
+    }
+    
+   public void setModel(Model model) {
+    	
+    	this.model = model;
+    	
+    }
+    
+}
